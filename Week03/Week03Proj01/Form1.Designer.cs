629d85de4f15a9c7554297c1043a1bc3c4bb1b9d
wxg115 <74805198+wxg115@users.noreply.github.com>
2025-03-19 12:30:44 +0900

    Add files via upload

---
+++ Week03/Week03Proj01/Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Week03Proj01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxData1 = new System.Windows.Forms.TextBox();
+            this.tbxData2 = new System.Windows.Forms.TextBox();
+            this.tbxData3 = new System.Windows.Forms.TextBox();
+            this.tbxData4 = new System.Windows.Forms.TextBox();
+            this.tbxData5 = new System.Windows.Forms.TextBox();
+            this.chkAdd = new System.Windows.Forms.CheckBox();
+            this.chkSub = new System.Windows.Forms.CheckBox();
+            this.chkMul = new System.Windows.Forms.CheckBox();
+            this.chkDiv = new System.Windows.Forms.CheckBox();
+            this.chkOption = new System.Windows.Forms.CheckBox();
+            this.pnlRadioGroup = new System.Windows.Forms.Panel();
+            this.rbtDiv = new System.Windows.Forms.RadioButton();
+            this.rbtMul = new System.Windows.Forms.RadioButton();
+            this.rbtSub = new System.Windows.Forms.RadioButton();
+            this.rbtAdd = new System.Windows.Forms.RadioButton();
+            this.btnProcess01 = new System.Windows.Forms.Button();
+            this.btnProcess02 = new System.Windows.Forms.Button();
+            this.btnProcess03 = new System.Windows.Forms.Button();
+            this.btnProcess04 = new System.Windows.Forms.Button();
+            this.btnProcess05 = new System.Windows.Forms.Button();
+            this.btnProcess06 = new System.Windows.Forms.Button();
+            this.btnProcess07 = new System.Windows.Forms.Button();
+            this.btnProcess08 = new System.Windows.Forms.Button();
+            this.btnProcess09 = new System.Windows.Forms.Button();
+            this.btnProcess10 = new System.Windows.Forms.Button();
+            this.cmbBeverage = new System.Windows.Forms.ComboBox();
+            this.chkIce = new System.Windows.Forms.CheckBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pnlRadioGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbxData1
+            // 
+            this.tbxData1.Location = new System.Drawing.Point(12, 12);
+            this.tbxData1.Name = "tbxData1";
+            this.tbxData1.Size = new System.Drawing.Size(100, 21);
+            this.tbxData1.TabIndex = 0;
+            // 
+            // tbxData2
+            // 
+            this.tbxData2.Location = new System.Drawing.Point(118, 12);
+            this.tbxData2.Name = "tbxData2";
+            this.tbxData2.Size = new System.Drawing.Size(100, 21);
+            this.tbxData2.TabIndex = 0;
+            // 
+            // tbxData3
+            // 
+            this.tbxData3.Location = new System.Drawing.Point(224, 12);
+            this.tbxData3.Name = "tbxData3";
+            this.tbxData3.Size = new System.Drawing.Size(100, 21);
+            this.tbxData3.TabIndex = 0;
+            // 
+            // tbxData4
+            // 
+            this.tbxData4.Location = new System.Drawing.Point(330, 12);
+            this.tbxData4.Name = "tbxData4";
+            this.tbxData4.Size = new System.Drawing.Size(100, 21);
+            this.tbxData4.TabIndex = 0;
+            // 
+            // tbxData5
+            // 
+            this.tbxData5.Location = new System.Drawing.Point(436, 12);
+            this.tbxData5.Name = "tbxData5";
+            this.tbxData5.Size = new System.Drawing.Size(100, 21);
+            this.tbxData5.TabIndex = 0;
+            // 
+            // chkAdd
+            // 
+            this.chkAdd.AutoSize = true;
+            this.chkAdd.Location = new System.Drawing.Point(13, 40);
+            this.chkAdd.Name = "chkAdd";
+            this.chkAdd.Size = new System.Drawing.Size(60, 16);
+            this.chkAdd.TabIndex = 1;
+            this.chkAdd.Text = "더하기";
+            this.chkAdd.UseVisualStyleBackColor = true;
+            // 
+            // chkSub
+            // 
+            this.chkSub.AutoSize = true;
+            this.chkSub.Location = new System.Drawing.Point(118, 40);
+            this.chkSub.Name = "chkSub";
+            this.chkSub.Size = new System.Drawing.Size(48, 16);
+            this.chkSub.TabIndex = 2;
+            this.chkSub.Text = "빼기";
+            this.chkSub.UseVisualStyleBackColor = true;
+            // 
+            // chkMul
+            // 
+            this.chkMul.AutoSize = true;
+            this.chkMul.Location = new System.Drawing.Point(224, 39);
+            this.chkMul.Name = "chkMul";
+            this.chkMul.Size = new System.Drawing.Size(60, 16);
+            this.chkMul.TabIndex = 3;
+            this.chkMul.Text = "곱하기";
+            this.chkMul.UseVisualStyleBackColor = true;
+            // 
+            // chkDiv
+            // 
+            this.chkDiv.AutoSize = true;
+            this.chkDiv.Location = new System.Drawing.Point(330, 40);
+            this.chkDiv.Name = "chkDiv";
+            this.chkDiv.Size = new System.Drawing.Size(60, 16);
+            this.chkDiv.TabIndex = 4;
+            this.chkDiv.Text = "나누기";
+            this.chkDiv.UseVisualStyleBackColor = true;
+            // 
+            // chkOption
+            // 
+            this.chkOption.AutoSize = true;
+            this.chkOption.Location = new System.Drawing.Point(436, 40);
+            this.chkOption.Name = "chkOption";
+            this.chkOption.Size = new System.Drawing.Size(50, 16);
+            this.chkOption.TabIndex = 6;
+            this.chkOption.Text = "ENG";
+            this.chkOption.UseVisualStyleBackColor = true;
+            // 
+            // pnlRadioGroup
+            // 
+            this.pnlRadioGroup.Controls.Add(this.rbtDiv);
+            this.pnlRadioGroup.Controls.Add(this.rbtMul);
+            this.pnlRadioGroup.Controls.Add(this.rbtSub);
+            this.pnlRadioGroup.Controls.Add(this.rbtAdd);
+            this.pnlRadioGroup.Location = new System.Drawing.Point(12, 62);
+            this.pnlRadioGroup.Name = "pnlRadioGroup";
+            this.pnlRadioGroup.Size = new System.Drawing.Size(418, 24);
+            this.pnlRadioGroup.TabIndex = 7;
+            // 
+            // rbtDiv
+            // 
+            this.rbtDiv.AutoSize = true;
+            this.rbtDiv.Location = new System.Drawing.Point(318, 4);
+            this.rbtDiv.Name = "rbtDiv";
+            this.rbtDiv.Size = new System.Drawing.Size(59, 16);
+            this.rbtDiv.TabIndex = 1;
+            this.rbtDiv.TabStop = true;
+            this.rbtDiv.Text = "나누기";
+            this.rbtDiv.UseVisualStyleBackColor = true;
+            // 
+            // rbtMul
+            // 
+            this.rbtMul.AutoSize = true;
+            this.rbtMul.Location = new System.Drawing.Point(212, 3);
+            this.rbtMul.Name = "rbtMul";
+            this.rbtMul.Size = new System.Drawing.Size(59, 16);
+            this.rbtMul.TabIndex = 0;
+            this.rbtMul.TabStop = true;
+            this.rbtMul.Text = "곱하기";
+            this.rbtMul.UseVisualStyleBackColor = true;
+            // 
+            // rbtSub
+            // 
+            this.rbtSub.AutoSize = true;
+            this.rbtSub.Location = new System.Drawing.Point(106, 4);
+            this.rbtSub.Name = "rbtSub";
+            this.rbtSub.Size = new System.Drawing.Size(47, 16);
+            this.rbtSub.TabIndex = 1;
+            this.rbtSub.TabStop = true;
+            this.rbtSub.Text = "빼기";
+            this.rbtSub.UseVisualStyleBackColor = true;
+            // 
+            // rbtAdd
+            // 
+            this.rbtAdd.AutoSize = true;
+            this.rbtAdd.Location = new System.Drawing.Point(4, 4);
+            this.rbtAdd.Name = "rbtAdd";
+            this.rbtAdd.Size = new System.Drawing.Size(59, 16);
+            this.rbtAdd.TabIndex = 0;
+            this.rbtAdd.TabStop = true;
+            this.rbtAdd.Text = "더하기";
+            this.rbtAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess01
+            // 
+            this.btnProcess01.Location = new System.Drawing.Point(12, 92);
+            this.btnProcess01.Name = "btnProcess01";
+            this.btnProcess01.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess01.TabIndex = 8;
+            this.btnProcess01.Text = "연산1";
+            this.btnProcess01.UseVisualStyleBackColor = true;
+            this.btnProcess01.Click += new System.EventHandler(this.btnProcess01_Click);
+            // 
+            // btnProcess02
+            // 
+            this.btnProcess02.Location = new System.Drawing.Point(93, 92);
+            this.btnProcess02.Name = "btnProcess02";
+            this.btnProcess02.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess02.TabIndex = 9;
+            this.btnProcess02.Text = "연산2";
+            this.btnProcess02.UseVisualStyleBackColor = true;
+            this.btnProcess02.Click += new System.EventHandler(this.btnProcess02_Click);
+            // 
+            // btnProcess03
+            // 
+            this.btnProcess03.Location = new System.Drawing.Point(174, 92);
+            this.btnProcess03.Name = "btnProcess03";
+            this.btnProcess03.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess03.TabIndex = 8;
+            this.btnProcess03.Text = "연산3";
+            this.btnProcess03.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess04
+            // 
+            this.btnProcess04.Location = new System.Drawing.Point(255, 92);
+            this.btnProcess04.Name = "btnProcess04";
+            this.btnProcess04.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess04.TabIndex = 9;
+            this.btnProcess04.Text = "연산4";
+            this.btnProcess04.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess05
+            // 
+            this.btnProcess05.Location = new System.Drawing.Point(336, 92);
+            this.btnProcess05.Name = "btnProcess05";
+            this.btnProcess05.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess05.TabIndex = 9;
+            this.btnProcess05.Text = "연산5";
+            this.btnProcess05.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess06
+            // 
+            this.btnProcess06.Location = new System.Drawing.Point(12, 121);
+            this.btnProcess06.Name = "btnProcess06";
+            this.btnProcess06.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess06.TabIndex = 8;
+            this.btnProcess06.Text = "연산6";
+            this.btnProcess06.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess07
+            // 
+            this.btnProcess07.Location = new System.Drawing.Point(93, 121);
+            this.btnProcess07.Name = "btnProcess07";
+            this.btnProcess07.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess07.TabIndex = 9;
+            this.btnProcess07.Text = "연산7";
+            this.btnProcess07.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess08
+            // 
+            this.btnProcess08.Location = new System.Drawing.Point(174, 121);
+            this.btnProcess08.Name = "btnProcess08";
+            this.btnProcess08.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess08.TabIndex = 8;
+            this.btnProcess08.Text = "연산8";
+            this.btnProcess08.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess09
+            // 
+            this.btnProcess09.Location = new System.Drawing.Point(255, 121);
+            this.btnProcess09.Name = "btnProcess09";
+            this.btnProcess09.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess09.TabIndex = 9;
+            this.btnProcess09.Text = "연산9";
+            this.btnProcess09.UseVisualStyleBackColor = true;
+            // 
+            // btnProcess10
+            // 
+            this.btnProcess10.Location = new System.Drawing.Point(336, 121);
+            this.btnProcess10.Name = "btnProcess10";
+            this.btnProcess10.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess10.TabIndex = 9;
+            this.btnProcess10.Text = "연산10";
+            this.btnProcess10.UseVisualStyleBackColor = true;
+            // 
+            // cmbBeverage
+            // 
+            this.cmbBeverage.FormattingEnabled = true;
+            this.cmbBeverage.Items.AddRange(new object[] {
+            "아메리카노",
+            "카페라떼",
+            "플랫화이트"});
+            this.cmbBeverage.Location = new System.Drawing.Point(12, 150);
+            this.cmbBeverage.Name = "cmbBeverage";
+            this.cmbBeverage.Size = new System.Drawing.Size(121, 20);
+            this.cmbBeverage.TabIndex = 10;
+            // 
+            // chkIce
+            // 
+            this.chkIce.AutoSize = true;
+            this.chkIce.Location = new System.Drawing.Point(140, 153);
+            this.chkIce.Name = "chkIce";
+            this.chkIce.Size = new System.Drawing.Size(48, 16);
+            this.chkIce.TabIndex = 11;
+            this.chkIce.Text = "얼음";
+            this.chkIce.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(12, 184);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(776, 257);
+            this.lblResult.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.chkIce);
+            this.Controls.Add(this.cmbBeverage);
+            this.Controls.Add(this.btnProcess10);
+            this.Controls.Add(this.btnProcess05);
+            this.Controls.Add(this.btnProcess09);
+            this.Controls.Add(this.btnProcess04);
+            this.Controls.Add(this.btnProcess08);
+            this.Controls.Add(this.btnProcess03);
+            this.Controls.Add(this.btnProcess07);
+            this.Controls.Add(this.btnProcess02);
+            this.Controls.Add(this.btnProcess06);
+            this.Controls.Add(this.btnProcess01);
+            this.Controls.Add(this.pnlRadioGroup);
+            this.Controls.Add(this.chkOption);
+            this.Controls.Add(this.chkDiv);
+            this.Controls.Add(this.chkMul);
+            this.Controls.Add(this.chkSub);
+            this.Controls.Add(this.chkAdd);
+            this.Controls.Add(this.tbxData5);
+            this.Controls.Add(this.tbxData4);
+            this.Controls.Add(this.tbxData3);
+            this.Controls.Add(this.tbxData2);
+            this.Controls.Add(this.tbxData1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlRadioGroup.ResumeLayout(false);
+            this.pnlRadioGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxData1;
+        private System.Windows.Forms.TextBox tbxData2;
+        private System.Windows.Forms.TextBox tbxData3;
+        private System.Windows.Forms.TextBox tbxData4;
+        private System.Windows.Forms.TextBox tbxData5;
+        private System.Windows.Forms.CheckBox chkAdd;
+        private System.Windows.Forms.CheckBox chkSub;
+        private System.Windows.Forms.CheckBox chkMul;
+        private System.Windows.Forms.CheckBox chkDiv;
+        private System.Windows.Forms.CheckBox chkOption;
+        private System.Windows.Forms.Panel pnlRadioGroup;
+        private System.Windows.Forms.RadioButton rbtDiv;
+        private System.Windows.Forms.RadioButton rbtMul;
+        private System.Windows.Forms.RadioButton rbtSub;
+        private System.Windows.Forms.RadioButton rbtAdd;
+        private System.Windows.Forms.Button btnProcess01;
+        private System.Windows.Forms.Button btnProcess02;
+        private System.Windows.Forms.Button btnProcess03;
+        private System.Windows.Forms.Button btnProcess04;
+        private System.Windows.Forms.Button btnProcess05;
+        private System.Windows.Forms.Button btnProcess06;
+        private System.Windows.Forms.Button btnProcess07;
+        private System.Windows.Forms.Button btnProcess08;
+        private System.Windows.Forms.Button btnProcess09;
+        private System.Windows.Forms.Button btnProcess10;
+        private System.Windows.Forms.ComboBox cmbBeverage;
+        private System.Windows.Forms.CheckBox chkIce;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Week03/Week03Proj01/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week03Proj01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess01_Click(object sender, EventArgs e)
+        {
+            //배열의 가장 큰 특징: 고정길이
+            //배열 생성 시 사용할 길이를 정해놓고 시작해야 함
+            TextBox[] arrTbxData = new TextBox[5];
+
+            arrTbxData[0] = tbxData1;
+            arrTbxData[1] = tbxData2;
+            arrTbxData[2] = tbxData3;
+            arrTbxData[3] = tbxData4;
+            arrTbxData[4] = tbxData5;
+
+            //int[] arrIntData = new int[5];
+            //배열의 길이는 반드시 상수값(불변값)이 들어가야 함
+            //배열의 길이는 변하지 않음
+            //다른 배열의 길이로 사용해도 됨
+            //배열 요소의 값은
+            //struct는 struct의 기본값 (int는 0)
+            //class는 null이 기본값
+            int[] arrIntData = new int[arrTbxData.Length];
+
+            for (int i = 0; i < arrTbxData.Length; i++)
+            {
+                if (arrTbxData[i].Text != null){ //error
+                    arrIntData[i] = int.Parse(arrTbxData[i].Text);
+                }
+                else
+                {
+                    //arrIntData[i] = 0; 과 동일
+                }
+            }
+            int result = 0;
+            if (rbtAdd.Checked)
+            {
+                for (int i = 0; i < arrIntData.Length; i++)
+                {
+                    result += arrIntData[i];
+                }
+            }
+            else if (rbtSub.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result -= arrIntData[i];
+                }
+            }
+            else if (rbtMul.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result *= arrIntData[i];
+                }
+            }
+            else if (rbtDiv.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    if (arrIntData[i] == 0)
+                    {
+                        arrTbxData[i].Focus();
+                        MessageBox.Show("0이 입력됨");
+                    }
+                    result /= arrIntData[i];
+                }
+            }
+            else
+            {
+                MessageBox.Show("연산을 선택하세요");
+                return; //메소드를 종료하고 호출한 곳으로 돌아감
+            }
+
+                lblResult.Text = result.ToString();
+        }
+
+        private void btnProcess02_Click(object sender, EventArgs e)
+        {
+            //배열의 가장 큰 특징: 고정길이
+            //배열 생성 시 사용할 길이를 정해놓고 시작해야 함
+            TextBox[] arrTbxData = new TextBox[5];
+
+            arrTbxData[0] = tbxData1;
+            arrTbxData[1] = tbxData2;
+            arrTbxData[2] = tbxData3;
+            arrTbxData[3] = tbxData4;
+            arrTbxData[4] = tbxData5;
+
+            //int[] arrIntData = new int[5];
+            //배열의 길이는 반드시 상수값(불변값)이 들어가야 함
+            //배열의 길이는 변하지 않음
+            //다른 배열의 길이로 사용해도 됨
+            //배열 요소의 값은
+            //struct는 struct의 기본값 (int는 0)
+            //class는 null이 기본값
+            int[] arrIntData = new int[arrTbxData.Length];
+
+            for (int i = 0; i < arrTbxData.Length; i++)
+            {
+                if (false == string.IsNullOrEmpty(arrTbxData[i].Text)) {
+                    arrIntData[i] = int.Parse(arrTbxData[i].Text);
+                }
+                else
+                {
+                    //arrIntData[i] = 0; 과 동일
+                }
+            }
+            int result = 0;
+            if (rbtAdd.Checked)
+            {
+                for (int i = 0; i < arrIntData.Length; i++)
+                {
+                    result += arrIntData[i];
+                }
+            }
+            else if (rbtSub.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result -= arrIntData[i];
+                }
+            }
+            else if (rbtMul.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    result *= arrIntData[i];
+                }
+            }
+            else if (rbtDiv.Checked)
+            {
+                result = arrIntData[0];
+                for (int i = 1; i < arrIntData.Length; i++)
+                {
+                    if (arrIntData[i] == 0)
+                    {
+                        arrTbxData[i].Focus();
+                        MessageBox.Show("0이 입력됨");
+                    }
+                    result /= arrIntData[i];
+                }
+            }
+            else
+            {
+                MessageBox.Show("연산을 선택하세요");
+                return; //메소드를 종료하고 호출한 곳으로 돌아감
+            }
+
+            lblResult.Text = result.ToString();
+        }
+    }
+
+}
