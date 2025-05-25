55b666f25d9d248723de62354fbc148b9271b28c
wxg115 <74805198+wxg115@users.noreply.github.com>
2025-05-25 09:03:41 +0900

    Add files via upload

---
+++ Week12/Week09Homework/Department.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09Homework
+{
+    class Department : object
+    {
+        private string _code;
+
+        public string Code
+        {
+            get { return _code; }
+            private set {
+                if (false == string.IsNullOrEmpty(value)) {
+                    _code = value;
+                } else {
+                    throw new Exception("Value is Empty");
+                }
+            }
+        }
+
+        public string GetCode()
+        {
+            return _code;
+        }
+
+        private void SetCode(string value)
+        {
+            if (false == string.IsNullOrEmpty(value)) {
+                _code = value;
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            private set {
+                if (false == string.IsNullOrEmpty(value)
+                    && value.Length >= 2) {
+                    _name = value;
+                } else {
+                    throw new Exception("Value is Empty");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{Code}] {Name}";
+        }
+
+        public string Record
+        {
+            get { return $"{Code}|{Name}"; }
+        }
+
+#if false
+        public Department()
+        {
+        
+        }
+#endif
+
+        public Department(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+
+        public static Department Restore(string record)
+        {
+            Department dept = null;
+            var splitdatas = record.Trim().Split(new char[] { '|' });
+            dept = new Department(splitdatas[0], splitdatas[1]);
+            return dept;
+        }
+    }
+}
---
+++ Week12/Week09Homework/Grade.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09Homework
+{
+    class Grade
+    {
+        public const int MAX_GRADE_COUNT = 9;
+
+        private string _studentNumber;
+        public string StudentNumber
+        {
+            get { return _studentNumber; }
+        }
+
+        private List<double> _scores = new List<double>();
+
+        public Grade(string studentNumber)
+        {
+            _studentNumber = studentNumber;
+        }
+
+        public int Count()
+        {
+            return _scores.Count;
+        }
+        public double Get(int i)
+        {
+            return _scores[i];
+        }
+
+        public double Average()
+        {
+            if (this._scores.Count == 0) {
+                return -1.0;
+            }
+
+            double sum = 0.0;
+            foreach (var score in this._scores) {
+                sum += score;
+            }
+            return sum / _scores.Count;
+        }
+
+        public void Clear()
+        {
+            _scores.Clear();
+        }
+
+        public bool Add(double score)
+        {
+            if (_scores.Count >= Grade.MAX_GRADE_COUNT) {
+                return false;
+            }
+
+            _scores.Add(score);
+            return true;
+        }
+    }
+}
